b90c3c9d5b74d203f0be418e7517a99961912ca4
Ayan Sarkar <ags4000@hw.ac.uk>
2025-11-30 21:44:59 +0000

    Implémentation du calendrier pour trouveer un gp spécifique pour toutes les saisons

--- src/data_manager.py
+++ src/data_manager.py
@@ -4,10 +4,11 @@ import time
 import os
 
 
-# on récupère les dossiers (actuel, celui des données, et du CSV)
+# on récupère les dossiers (actuel, celui des données, et des CSV)
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(os.path.dirname(CURRENT_DIR), "data")
-CSV_PATH = os.path.join(DATA_DIR, "f1_data_complete.csv")
+RESULTS_CSV_PATH = os.path.join(DATA_DIR, "f1_data_complete.csv")
+CALENDAR_CSV_PATH = os.path.join(DATA_DIR, "races_calendar.csv")
 
 # créer le dossier data si inexistant
 if not os.path.exists(DATA_DIR):
@@ -37,7 +38,7 @@ def _fetch_race_result(url):
     return None
 
 
-# fonction principale 1 : update le csv, et permet de filtrer les données qu'on veut par saison et course
+#  Partie 1 : télécharge les résultats, update le csv, et permet de filtrer les données qu'on veut par saison et course
 def update_database(start_year=2019, end_year=2025):
     # télécharge les données et écrase le CSV
     print(f"Mise à jour de la base de donnée ({start_year}-{end_year})...")
@@ -57,12 +58,39 @@ def update_database(start_year=2019, end_year=2025):
     if all_races:
         df_final = pd.concat(all_races, ignore_index=True)
         df_final.to_csv(CSV_PATH, index=False)
-        print(f"Base de données sauvegardée dans : {CSV_PATH}")
+        print(f"Base de données sauvegardée dans : {RESULTS_CSV_PATH}")
     else:
         print("Aucune donnée récupérée.")
 
 
-# fonction principale 2 : chargement des données
+# Partie 2 : téléchargement du calendrier
+def update_calendar(start_year=2019, end_year=2025):
+    # on récupère la liste de toutes les courses
+    print(f"Mise à jour du calendrier ({start_year}-{end_year})...")
+    all_schedules = []
+    for year in range(start_year, end_year+1):
+        url = f"https://api.jolpi.ca/ergast/f1/{year}.json"
+        try:
+            response = requests.get(url)
+            data = response.json()
+            races = data["MRData"]["RaceTable"]["Races"]
+            for race in races:
+                all_schedules.append({
+                    "year": int(race["season"]),
+                    "round": int(race["round"]),
+                    "raceName": race["raceName"],
+                    "circuitId": race["Circuit"]["circuitId"],
+                    "date": race["date"]
+                })
+            print(f"  -> Saison {year} : OK")
+        except Exception as e:
+            print(f"  Erreur {year} : {e}")
+    df_calendar = pd.DataFrame(all_schedules)
+    df_calendar.to_csv(CALENDAR_CSV_PATH, index=False)
+    print(f"Calendrier enregistré dans : {CALENDAR_CSV_PATH}")
+
+
+# Partie 3 : chargement des donnée
 def load_data():
     # On charge le CSV s'il existe, sinon on propose de le télécharger
     if os.path.exists(CSV_PATH):
@@ -75,3 +103,17 @@ def load_data():
             return pd.read_csv(CSV_PATH)
         else:
             return None
+
+# cherche dans le calendrier csv et renvoie un dictionnaire {année: round}
+def get_rounds_for_race(race_name_keyword):
+    if not os.path.exists(CALENDAR_CSV_PATH):
+        print("Calendrier introuvable.\nTéléchargement...")
+        update_calendar()
+    df = pd.read_csv(CALENDAR_CSV_PATH)
+    # on filtre avec notre paramètre d'entrée
+    filtered = df[df["raceName"].str.contains(race_name_keyword, case=False, na=False)]
+    if filtered.empty:
+        print(f'Aucune course trouvée avec le nom "{race_name_keyword}".')
+        return {}
+    # on créé le dictionnaire final
+    return dict(zip(filtered["year"], filtered["round"]))
