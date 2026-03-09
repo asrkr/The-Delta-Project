edb13beae1c2b194f30a3b0d96364655487fb4c7
Ayan Sarkar <ags4000@hw.ac.uk>
2026-03-09 09:00:15 +0100

    chore: formatting

--- update_manager.py
+++ update_manager.py
@@ -28,51 +28,43 @@ if __name__ == "__main__":
         y2 = int(input("End year (latest season): "))
         update_database(y1, y2)
 
-
     elif choice == 2:
         print("Ergast results update.")
         y = int(input("Season to update (from 2001 to latest season): "))
         update_database(y, y)
 
-    
     elif choice == 3:
         print("Calendar update.")
         y1 = int(input("Start year (recommended: 2001): "))
         y2 = int(input("End year (latest season): "))
         update_calendar(y1, y2)
-    
-    
+
     elif choice == 4:
         print("FastF1 features update.")
         y1 = int(input("Start year (not prior than 2018): "))
         y2 = int(input("End year (latest season): "))
         extract_fastf1_features(y1, y2)
 
-
     elif choice == 5:
         print("FastF1 features update.")
         y = int(input("Season to update (no prior than 2018): "))
         extract_fastf1_features(y, y)
-    
 
     elif choice == 6:
         y = int(input(("Year: ")))
         rnd = int(input("Round: "))
         update_latest_qualifying(y, rnd)
-    
 
     elif choice == 7:
         print("Sprint results update.")
         y1 = int(input("Start year (sprints started in 2021): "))
         y2 = int(input("End year (latest season): "))
         update_sprint_data(y1, y2)
-    
 
     elif choice == 8:
         print("Sprint results update.")
         y = int(input("Season to update (sprints started in 2021): "))
         update_sprint_data(y, y)
-
 
     elif choice == 9:
         print("Full update.")
@@ -84,12 +76,10 @@ if __name__ == "__main__":
         update_calendar(y1, y3)
         extract_fastf1_features(y2, y3)
         update_sprint_data(2021, y3)
-        update_latest_qualifying(y3, rnd)
-    
+        update_latest_qualifying(y3, rnd)    
 
     elif choice == 10:
         exit()
 
-    
     else:
         print("Invalid choice")
